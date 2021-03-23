127e34e92892ca4076f82e184d11cd9e82774ab4
Jesse Einfalt <jeinfalt@quantummetric.com>
2021-03-22 20:32:53 -0600

    Add Testcafe and basic tests

---
+++ tests/smoketests.test.js
@@ -0,0 +1,15 @@
+import { Selector } from 'testcafe';
+
+fixture `Smoketests`
+    .page `http://localhost:8000/`;
+
+test('Smoketests', async t => {
+    const title = Selector("div[class='inner']");
+    await t.expect(title.find('h2').innerText).eql('MADELEINE EINFALT');
+    await t.expect(title.find('p').innerText).eql('BRINGING YOUR ARTISTIC VISION TO LIFE');
+
+    const logo = Selector("div[class='image']");
+    await t.expect(logo.exists).ok();
+
+    await t.debug();
+});
