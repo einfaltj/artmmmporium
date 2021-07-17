73df36a3b6f3356d0624451ddfbc6b9b163bf8e2
Jesse Einfalt <einfaltj88@gmail.com>
2021-07-17 12:39:53 -0600

    update form for gatsby || add /thanks route

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,22 +1,67 @@
+import { navigate } from 'gatsby';
 import React from 'react';
 
-export default function MuralProjects() {
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+}
+
+export default function ContactForm() {
+  const [state, setState] = React.useState({});
+
+  const handleChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch(error => alert(error));
+  };
+
   return (
     <section id="cta" className="wrapper style4 special">
-      <form name="contact" method="POST" data-netlify="true" data-netlify-recaptcha="true">
-        <p>
+      <form
+        name="contact"
+        method="POST"
+        action="/thanks/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        data-netlify-recaptcha="true"
+        onSubmit={handleSubmit}
+      >
+        <input type="hidden" name="contact" value="contact" />
+        <p hidden>
           <label>
-            Your Name: <input type="text" name="name" />
+            Don’t fill this out:{' '}
+            <input name="bot-field" onChange={handleChange} />
           </label>
         </p>
         <p>
           <label>
-            Your Email: <input type="email" name="email" />
+            Your Name: <input type="text" name="name" onChange={handleChange} />
           </label>
         </p>
         <p>
           <label>
-            Subject Line: <input type="text" name="subject" />
+            Your Email:{' '}
+            <input type="email" name="email" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Subject Line:{' '}
+            <input type="text" name="subject" onChange={handleChange} />
           </label>
         </p>
         <p>
@@ -24,7 +69,7 @@ export default function MuralProjects() {
             Subject: <textarea name="message"></textarea>
           </label>
         </p>
-        <div data-netlify-recaptcha="true"/>
+        <div data-netlify-recaptcha="true" />
         <p>
           <button type="submit">Send</button>
         </p>
---
+++ src/pages/thanks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+const IndexPage = () => (
+  <Layout fullMenu>
+    <article id="main">
+      <h1>Thank you!</h1>
+      <p>This is a custom thank you page for form submissions</p>
+    </article>
+  </Layout>
+);
+
+export default IndexPage;
