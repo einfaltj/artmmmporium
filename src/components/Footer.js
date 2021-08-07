7b934b8a9576f89bd33be216d38ac3f6943760b2
Jesse Einfalt <einfaltj88@gmail.com>
2021-08-07 14:12:27 -0600

    Refactor ContactIcons to seperate componet | opens new window on click

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import config from '../../config';
+import ContactIcons from './ContactIcons';
 
 let year = new Date().getFullYear();
 
 export default function Footer() {
   return (
     <footer id="footer">
-      <ul className="icons">
-        {config.socialLinks.map(social => {
-          const { style, icon, name, url } = social;
-          return (
-            <li key={url}>
-              <a href={url} className={`icon ${style} ${icon}`}>
-                <span className="label">{name}</span>
-              </a>
-            </li>
-          );
-        })}
-      </ul>
+      <ContactIcons/>
       <p className="copyright">&copy; Artmmmporium {year}</p>
     </footer>
   );
